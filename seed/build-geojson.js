8f721a0d678db1418fd2f79e36e562c257e4c8a2
Ryan Coughlin <ryan@ryancoughlin.com>
2018-03-08 22:22:24 -0500

    Update HAPI syntax to v17 and add in nearby station endpoint

--- seed/build-geojson.js
+++ seed/build-geojson.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 const GeoJSON = require('geojson')
 
-const makeGeoJSON = stations => {
-  return GeoJSON.parse(stations, {
+const SOURCE_FILE = './seed/data/all-stations.json'
+const OUTPUT_FILE = './seed/data/stations.geojson'
+
+const makeGeoJSON = json => {
+  return GeoJSON.parse(json.stations, {
     Point: ['location.latitude', 'location.longitude'],
     include: ['name', 'id']
   })
@@ -10,18 +13,14 @@ const makeGeoJSON = stations => {
 
 const writeFile = data => {
   if (data) {
-    fs.writeFile(
-      './seed/data/stations.geojson',
-      JSON.stringify(data, 0, 4),
-      function(err) {
-        console.log('Successfully written!')
-      }
-    )
+    fs.writeFile(OUTPUT_FILE, JSON.stringify(data, 0, 4), function(err) {
+      console.log('Successfully written!')
+    })
   } else {
     console.log('No data to write')
   }
 }
 
-const json = JSON.parse(fs.readFileSync('./seed/data/stations.json', 'utf8'))
+const json = JSON.parse(fs.readFileSync(SOURCE_FILE, 'utf8'))
 const geojson = makeGeoJSON(json)
 writeFile(geojson)
