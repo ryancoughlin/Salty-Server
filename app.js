4f5dbd19269dfd8000cd1f594c80c9c5ef2c2617
ryancoughlin <ryan@ryancoughlin.com>
2021-03-03 18:26:40 +0000

    app

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-const port = 3000
+const port = 3000;
 require("dotenv").config();
 
 const defaultRoutes = require("./routes")();
 
-mongoose 
+mongoose
   .connect(process.env.MONGO_URL, {
-	  useNewUrlParser: true,
-	  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
-	console.log("Database connection Success!");
+    console.log("Database connection Success!");
   })
   .catch((err) => {
-	console.error("Mongo Connection Error", err);
+    console.error("Mongo Connection Error", err);
   });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const allowedOrigins = ['http://localhost:3000'];
-app.use(cors({
-	origin: function (origin, callback) {
-		if (!origin) return callback(null, true);
-		if (allowedOrigins.indexOf(origin) === -1) {
-			const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-			return callback(new Error(msg), false);
-		}
-		return callback(null, true);
-	},
-	credentials: true,
-}));
+const allowedOrigins = ["http://localhost:5000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api", defaultRoutes);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
